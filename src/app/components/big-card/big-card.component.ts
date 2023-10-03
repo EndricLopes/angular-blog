024cad23c614fc925815c5aa27e6d233e68e6267
Endric Lopes <endricayl@gmail.com>
2023-10-03 11:48:04 -0300

    antes do desafio

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -13,6 +13,8 @@ export class BigCardComponent implements OnInit {
   cardTitle:string= ""
   @Input()
   cardDescriptiom:string=""
+  @Input()
+  Id:string="0"
 
   constructor() { }
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+    "id":"1",
+    "title": "OPPENHAIMER FATUROU",
+    "description": "tao ganhando muito dinheiro, muito mesmo.",
+    "photoCover": "https://cdn.jornaldebrasilia.com.br/wp-content/uploads/2023/07/24090742/Cillian-Murphy-com-visual-de-Oppenheimer-2.jpg"
+    },
+    {
+        "id":"2",
+        "title": "Batman Cavaleiro das trevas",
+        "description": "muito bom, parabens, bom demais",
+         "photoCover": "https://f.i.uol.com.br/fotografia/2018/01/19/15163996455a626c1ddf040_1516399645_3x2_md.jpg"
+    }
+        ]
